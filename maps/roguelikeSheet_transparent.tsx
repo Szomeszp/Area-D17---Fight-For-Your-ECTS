176f6b3bb39222d680a426d385c3a1cd26bb8f58
Przemysław Kolaszyński <38491108+Szomeszp@users.noreply.github.com>
2021-04-26 22:50:58 +0200

    fix

--- maps/roguelikeSheet_transparent.tsx
+++ maps/roguelikeSheet_transparent.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="roguelikeSheet_transparent" tilewidth="32" tileheight="32" spacing="2" tilecount="1767" columns="57">
- <image source="../img/roguelikeSheet_transparent.png" trans="000000" width="1936" height="1052"/>
+ <image source="../img/roguelikeSheet_transparent.png" width="1936" height="1052"/>
 </tileset>
